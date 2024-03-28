fb55e9b95a1abdf3acc86e9f30f2b67362f96c48
CodeBoy-0223 <code20230223@163.com>
2024-03-28 21:11:12 +0800

    feat: 完成TodoItem中的功能

--- src/components/TodoList/TodoItem/index.jsx
+++ src/components/TodoList/TodoItem/index.jsx
@@ -1,14 +1,58 @@
 import { Component } from 'react'
 
 export default class TodoItem extends Component {
+	// handler - 修改done状态
+	handleChange = id => {
+		return () => {
+			// 此处需要修改父组件中的数据, 为父子组件通信 -- 父组件给子组件传递props, 并且值为一个函数
+			this.props.changeDone(id)
+		}
+	}
+
+	// handler - 删除指定todo
+	handleDelete = id => {
+		return () => {
+			this.props.deleteTodo(id)
+		}
+	}
+
+	// handler - 指定todo切换至编辑模式
+	handleEdit = id => {
+		return () => {
+			this.props.changeToEdit(id)
+		}
+	}
+
+	// handler - 保存指定todo
+	handleSave = id => {
+		return event => {
+			if (event.type === 'keyup' && event.key !== 'Enter') return
+			const value = event.target.value.trim()
+			if (value.length < 2) return
+			this.props.saveTodo(id, value)
+		}
+	}
+
 	render() {
+		// 获取 props
+		const { id, content, done, isEdit } = this.props
+
 		return (
-			<li className='todo-item'>
-				<div className='view'>
-					<input className='toggle' type='checkbox' />
-					<label>task-1</label>
-					<button className='destroy'></button>
+			<li className={`todo-item${isEdit ? ' editing' : ''}`}>
+				<div className='view' onDoubleClick={this.handleEdit(id)}>
+					<input className='toggle' type='checkbox' checked={done} onChange={this.handleChange(id)} />
+					<label>{content}</label>
+					<button className='destroy' onClick={this.handleDelete(id)}></button>
 				</div>
+				{isEdit && (
+					<input
+						className='edit'
+						autoFocus
+						defaultValue={content}
+						onBlur={this.handleSave(id)}
+						onKeyUp={this.handleSave(id)}
+					/>
+				)}
 			</li>
 		)
 	}
--- src/components/TodoList/index.jsx
+++ src/components/TodoList/index.jsx
@@ -6,9 +6,63 @@ export default class TodoList extends Component {
 	// TodoMvc应用所依赖的状态只在TodoList组件中进行展示, 而在其他组件中只是会修改它的值. 因此可以把状态定义在此处
 	state = {
 		todoList: [
-			{ id: '001', content: '学习react', done: false },
-			{ id: '002', content: '学习AI知识', done: false },
+			{ id: '001', content: '学习react', done: false, isEdit: false },
+			{ id: '002', content: '学习AI知识', done: false, isEdit: false },
 		],
+	}
+
+	// 修改指定todo的done状态
+	changeDone = id => {
+		this.setState({
+			todoList: this.state.todoList.map(todo => {
+				if (todo.id === id) {
+					todo.done = !todo.done
+				}
+				return todo
+			}),
+		})
+	}
+
+	// 修改所有todo的done状态
+	changeAllDone = () => {
+		this.setState({
+			todoList: this.state.todoList.map(todo => {
+				todo.done = !todo.done
+				return todo
+			}),
+		})
+	}
+
+	// 删除指定todo
+	deleteTodo = id => {
+		this.setState({
+			todoList: this.state.todoList.filter(todo => todo.id !== id),
+		})
+	}
+
+	// 更改指定todo的编辑状态
+	changeToEdit = id => {
+		this.setState({
+			todoList: this.state.todoList.map(todo => {
+				if (todo.id === id) {
+					todo.isEdit = true
+				}
+				return todo
+			}),
+		})
+	}
+
+	// 保存指定todo
+	saveTodo = (id, content) => {
+		this.setState({
+			todoList: this.state.todoList.map(todo => {
+				if (todo.id === id) {
+					todo.content = content
+					todo.isEdit = false
+				}
+				return todo
+			}),
+		})
 	}
 
 	render() {
@@ -18,15 +72,22 @@ export default class TodoList extends Component {
 		return (
 			<main className='main'>
 				<div className='toggle-all-container'>
-					<input className='toggle-all' type='checkbox' />
-					<label className='toggle-all-label' htmlFor='toggle-all'>
+					<input id='toggle-all' className='toggle-all' type='checkbox' />
+					<label className='toggle-all-label' htmlFor='toggle-all' onClick={this.changeAllDone}>
 						Mark all as complete
 					</label>
 				</div>
 				<ul className='todo-list'>
 					{/* 使用状态 - todoList */}
 					{todoList.map(todo => (
-						<TodoItem key={todo.id} {...todo} />
+						<TodoItem
+							key={todo.id}
+							{...todo}
+							changeDone={this.changeDone}
+							deleteTodo={this.deleteTodo}
+							changeToEdit={this.changeToEdit}
+							saveTodo={this.saveTodo}
+						/>
 					))}
 				</ul>
 			</main>
